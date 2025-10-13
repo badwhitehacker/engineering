e42daae7b0b2a1800901afb37253b5d4e7f14317
baddas <40858660+badwhitehacker@users.noreply.github.com>
2025-10-13 20:49:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-    baseUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/',
-    firstCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/checker.php',
-    baseCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com/dnsChecker.php'
+    baseUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com',
+    firstCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com',
+    baseCheckUrl: 'https://mediumaquamarine-anteater-755577.hostingersite.com'
 };
 
 // Fallback for different localhost setups
@@ -10,3 +10,4 @@ if (typeof config.firstCheckUrl === 'undefined') {
     config.baseCheckUrl = '/dnsChecker.php';
 
 }
+
